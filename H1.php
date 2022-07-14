46ba60ce528cfad25d78f53208097d6cd7eec1a9
Charles.K.Muia@gmail.com <Charles.K.Muia@gmail.com>
2022-07-14 12:51:49 +0300

    Lawfirm_1

---
+++ H1.php
@@ -0,0 +1,274 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="" href="Pics/Logo1.jpg">
+    <link rel="stylesheet" href="Style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Advent+Pro&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <title> Muia Mwanzia & Co. Advocates </title>
+</head>
+<body>
+
+<button onclick="topFunction()" id="Scroll-button" title="Go to top">Top</button>
+
+
+<!--                Header sections starts here-->
+<section class="Header1">
+    <hr class="closure">
+
+    <nav>
+        <a href="H1.php"><img src="Pics/Logo1.jpg" alt="" height="100"></a>
+        <div class="logo">
+            <ul>
+<!--                <li><a href="">Muia Mwanzia <br> <span> & </span>  <br> Co. Advocates</a></li>-->
+                                <li><a href="H1.php"> Muia Mwanzia & Co. Advocates <br>
+                                        <span> - justice be our shield and defender - </span></a></li>
+            </ul>
+        </div>
+                <div class="nav-links" id="navlink">
+                    <i class="fa fa-times-circle-o" onclick="hideMenu()"></i>
+                    <ul>
+                        <li><a href="Index.php">Home</a></li>
+                        <li><a href="About.php">About US</a></li>
+                        <li><a href="Practise_A.php">Practice Areas</a></li>
+                        <li><a href="News.php">News</a></li>
+                        <li><a href="Our_Team.php">Our Team</a></li>
+                        <li><a href="Contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
+                            <i class="fa fa-bars" onclick="displayMenu()"></i>
+    </nav>
+            <div class="text">
+                <h1>Advocates of the High Court of Kenya</h1>
+                         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque excepturi facere hic inventore nihil perferendis quas tempora voluptas voluptate, voluptatum.
+                    <br>Architecto eos maiores officiis pariatur quo tempore velit veniam voluptates.</p>
+                <a class="Learn" href="About.php">Learn More</a>
+            </div>
+</section>
+
+<!--                    Practise section starts here-->
+        <section class="Practise">
+            <h1>Practise Areas</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem culpa, dolore eaque in inventore ipsa ipsam
+                        <br>non quas quisquam recusandae saepe! Animi cum ea explicabo impedit magni ratione sunt. </p>
+                                        <div class="row">
+                                            <div class="practise-col">
+                                                <h2>Commercial & Corporate Law</h2>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, <br> cumque eius exercitationem expedita fugit id nemo numquam odit perspiciatis quaerat.
+                                                <br>Alias cumque debitis distinctio magni maxime quaerat ut vel voluptates.</p>
+                                            </div>
+                                        <div class="practise-col">
+                                            <h2>Civil & Criminal Litigation</h2>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, <br>cumque eius exercitationem expedita fugit id nemo numquam odit perspiciatis quaerat.
+                                                <br>Alias cumque debitis distinctio magni maxime quaerat ut vel voluptates.</p>
+                                            </div>
+                                            <div class="practise-col">
+                                                <h2>Conveyancing & Arbitration</h2>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
+                                                    <br>cumque eius exercitationem expedita fugit id nemo numquam odit perspiciatis quaerat.
+                                                    <br>Alias cumque debitis distinctio magni maxime quaerat ut vel voluptates.</p>
+                                            </div>
+                                    </div>
+        </section>
+
+<!--                        Commitment section starts here-->
+                        <section class="Commitment">
+                                <h1>Our Commitment</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
+                                <br>cumque eius exercitationem expedita fugit id nemo numquam odit perspiciatis quaerat.
+                                Alias cumque debitis distinctio magni maxime quaerat ut vel voluptates.</p>
+                            <div class="row">
+                                <div class="commitment-col">
+                                    <img src="Pics/p-3.jpg" alt="" width="">
+                                    <div class="commitment-layer">
+                                            <h3>Professionalism</h3>
+                                    </div>
+                                </div>
+                            <div class="commitment-col">
+                                <img src="Pics/p-5.jpg" alt="">
+                                <div class="commitment-layer">
+                                    <h3>Security</h3>
+                                </div>
+                            </div>
+                                    <div class="commitment-col">
+                                        <img src="Pics/p-4.jpg" alt="">
+                                        <div class="commitment-layer">
+                                            <h3>Trust</h3>
+                                        </div>
+                                    </div>
+                         </div>
+                     </section>
+
+<!--                        News sections starts here-->
+            <section>
+                            <div class="nws">
+                                <h1>News</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
+                                    <br>cumque eius exercitationem expedita fugit id nemo numquam odit perspiciatis quaerat.
+                                    Alias cumque debitis distinctio magni maxime quaerat ut vel voluptates.</p>
+
+                                <div class="row">
+                                    <div class="nws-col">
+                                        <img src="Pics/US-Supreme.jpg" alt="">
+                                        <h3> Corridors of Justice </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae cupiditate debitis, doloribus ipsa iste laudantium reprehenderit soluta voluptate?
+                                            <br>Accusantium aspernatur culpa dicta doloremque eum minima optio quam ratione reiciendis.</p>
+                                        <a class="Learn-News" href="News.php"> read more </a>
+                                    </div>
+
+                                    <div class="nws-col">
+                                        <img src="Pics/Advocate.jpg" alt="">
+                                        <h3> Advocates Section </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae cupiditate debitis, doloribus ipsa iste laudantium reprehenderit soluta voluptate?
+                                            <br>Accusantium aspernatur culpa dicta doloremque eum minima optio quam ratione reiciendis.</p>
+                                        <a class="Learn-News" href="News.php"> read more </a>
+
+                                    </div>
+
+                                    <div class="nws-col">
+                                        <img src="Pics/US-Supreme.jpg" alt="">
+                                        <h3> International Scene </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae cupiditate debitis, doloribus ipsa iste laudantium reprehenderit soluta voluptate?
+                                            <br>Accusantium aspernatur culpa dicta doloremque eum minima optio quam ratione reiciendis.</p>
+                                        <a class="Learn-News" href="News.php"> read more </a>
+
+                                    </div>
+                                </div>
+                            </div>
+
+<!--                Client Testimonials starts here-->
+            </section>
+                    <section class="Client-testimonials">
+                                <h1> What Our Clients Say </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti error illum libero obcaecati, omnis reiciendis repellat sequi. Animi aut blanditiis consequatur
+                            <br>corporis dicta distnctio dolor modi voluptatem. Doloribus, laudantium, nostrum.</p>
+                                    <div class="row">
+                                        <div class="test-col">
+                                            <img src="Pics/DOG.jpg" alt="">
+                                            <div >
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut autem consequuntur, corporis delectus ducimus enim id,
+                                                </p>
+                                                    <h3>DOG</h3>
+                                                 <i class="fa fa-star"></i>
+                                                 <i class="fa fa-star"></i>
+                                                 <i class="fa fa-star"></i>
+                                                 <i class="fa fa-star"></i>
+                                                 <i class="fa fa-star-half-o"></i>
+                                            </div>
+                                        </div>
+<!--                                        <div class="test-col">-->
+<!--                                            <img src="Pics/DOG.jpg" alt="">-->
+<!--                                            <div >-->
+<!--                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut autem consequuntur, corporis delectus ducimus enim id,-->
+<!--                                                    <br>illo impedit magni maxime natus neque non omnis possimus quibusdam quidem quisquam rem totam.</p>-->
+<!--                                                <h3>DOG</h3>-->
+<!--                                            </div>-->
+<!--                                        </div>-->
+                                        <div class="test-col">
+                                            <img src="Pics/Prince-C.jpg" alt="">
+                                            <div>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut autem consequuntur, corporis delectus ducimus enim id,
+                                                    <br>illo imp.</p>
+                                                <h3>Prince Charles</h3>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star-o"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                    </section>
+
+<!--                    Conclusion section starts here-->
+                    <section class="Con">
+                        <h1> If you need Legal counsel, <br> advice or representation, look no further.</h1>
+                        <hr>
+                        <br>
+                        <a class="Learn" href="Contact.php">contact us</a>
+                    </section>
+
+<!--                Footer starts here-->
+                    <section class="Footer">
+                        <h4> Muia Mwanzia & Co. Advocates <br>
+                        - Justice Be Our Shield & Defender - <br>
+<!--                            <i class= "fa fa-gavel">  </i>-->
+                        </h4>
+
+
+                        <!--                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut, <br>-->
+<!--                            commodi consequatur corporis exercitationem, <br> ipsam iste maiores modi mollitia nostrum omnis pariatur-->
+<!--                            <br>-->
+<!--                        </p>-->
+
+                        <div class="Social2">
+                            <p class="Footer-Con"> - Advocates of The High Court of Kenya   <span class="dot"> </span>   Commissioner for Oaths   <span class="dot"> </span>   Legal Services - </p>
+                            <i class= "fa fa-phone"> <br> <span class="Soc"> +254 733 333 333 </span> </i>
+                            <span class="dot2"> </span>
+                            <i class= "fa fa-envelope"> <br> <span class="Soc"> muiamwanzia@yahoo.com </span>  </i>
+                            <span class="dot2"> </span>
+                            <i class= "fa fa-building"> <br>  <span class="Soc"> Edfri Green House 2nd Floor </span>  </i>
+                            <span class="dot2"> </span>
+                            <i class= "fa fa-street-view"> <br> <span class="Soc"> Ghana Street </span> </i>
+                            <span class="dot2"> </span>
+                            <i class= "fa fa-map-marker"> <br> <span class="Soc"> Meru County </span>  </i>
+                        </div>
+                                <div class="Social">
+                                    <a href="https://www.facebook.com/MuiaMwanziaAndCo.Advocates"><i class= "fa fa-facebook"></i></a>
+                                        <i class= "fa fa-twitter"></i>
+                                        <i class= "fa fa-linkedin"></i>
+                                        <i class= "fa fa-youtube"></i>
+                                        <i class= "fa fa-instagram"></i>
+                                </div>
+                        <p class="Credits"> Website Designed by <br> <a class="Cre" href="">Techware Scope Solutions</a>
+                            <br> &copy 2022. </p>
+                        <hr class="closure">
+                    </section>
+
+<!--------------JS for Toggle Menu for small screens----------------->
+                <script>
+                    var navlink = document.getElementById("navlink");
+                    function displayMenu(){
+                        navlink.style.right = "0";
+                    }
+                    function hideMenu(){
+                        navlink.style.right = "-250px";
+                    }
+                </script>
+
+<!--------------JS for Scroll-button----------------->
+
+<script>
+    //Get the button
+    var mybutton = document.getElementById("Scroll-button");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+</script>
+
+
+</body>
+</html>
