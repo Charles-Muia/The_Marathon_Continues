e7496d676113d2515a0fb7a6ee7e178e3fe68284
Charles.K.Muia@gmail.com <Charles.K.Muia@gmail.com>
2022-07-18 18:19:55 +0300

    Lawfirm_8

--- Contact.php
+++ Contact.php
@@ -52,56 +52,13 @@
 </section>
 
 <!--                Contact Us sections starts here-->
-<section class="find-us">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.8170172650057!2d37.65097611404908!3d0.0463069643908277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x178821e9062a5bed%3A0x63990a4615bd7da!2sGhana%20St%2C%20Meru!5e0!3m2!1sen!2ske!4v1658064641770!5m2!1sen!2ske"
-            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</section>
 
-<section class="find-us-sec">
-    <div class="row">
-        <div class="find-us-col">
-            <div>
-                <i class= "fa fa-home"></i>
-                <span>
-                    <h6>XYL Street</h6>
-                    <p> Meru County</p>
-                </span>
-            </div>
-            <div>
-                <i class= "fa fa-home"></i>
-                <span>
-                    <h6>XYL Street</h6>
-                    <p> Meru County</p>
-                </span>
-            </div>
-            <div>
-                <i class= "fa fa-home"></i>
-                <span>
-                    <h6>XYL Street</h6>
-                    <p> Meru County</p>
-                </span>
-            </div>
-            <div>
-                <i class= "fa fa-phone"></i>
-                <span>
-                    <h6>XYL Street</h6>
-                    <p> Meru County</p>
-                </span>
-            </div>
-            <div>
-                <i class= "fa fa-home"></i>
-                <span>
-                    <h6>XYL Street</h6>
-                    <p> Meru County</p>
-                </span>
-            </div>
-        </div>
-        <div class="find-us-col"></div>
+        <section class="find-us">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.8170172650057!2d37.65097611404908!3d0.0463069643908277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x178821e9062a5bed%3A0x63990a4615bd7da!2sGhana%20St%2C%20Meru!5e0!3m2!1sen!2ske!4v1658064641770!5m2!1sen!2ske"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
 
 
-    </div>
-
-</section>
 
 
 <!--                Footer starts here-->
@@ -137,6 +94,7 @@
         <br> &copy 2022. </p>
     <hr class="closure">
 </section>
+
 
 <!--------------JS for Toggle Menu for small screens----------------->
 <script>
